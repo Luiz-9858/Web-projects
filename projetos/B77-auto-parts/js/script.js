2f11dc9d4ce2505d3e90d0d4975cc94643bd55e3
Luiz-9858 <susananarcisio1@gmail.com>
2025-10-18 23:26:18 -0300

    refactor: update toggleMenu function for improved navigation display

--- projetos/B77-auto-parts/js/script.js
+++ projetos/B77-auto-parts/js/script.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
-    const menu = document.getElementById('navMenu');
-    menu.classList.toggle('active');
-}
+            const navMenu = document.getElementById('navMenu');
+            if (navMenu.style.display === 'block') {
+                navMenu.style.display = 'none';
+            } else {
+                navMenu.style.display = 'block';
+            }
+        }
