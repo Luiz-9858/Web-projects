1a9307e6b0b320ba62ea8b651ce486316042161b
Luiz-9858 <susananarcisio1@gmail.com>
2025-10-26 20:41:48 -0300

    refatoração: ajustar estilo CSS para consistência e melhorar a responsividade; corrigir função toggleMenu para exibir/ocultar o menu corretamente

--- projetos/B77-auto-parts/js/script.js
+++ projetos/B77-auto-parts/js/script.js
@@ -1,8 +1,14 @@
 function toggleMenu() {
-            const navMenu = document.getElementById('navMenu');
-            if (navMenu.style.display === 'block') {
-                navMenu.style.display = 'none';
-            } else {
-                navMenu.style.display = 'block';
-            }
-        }
+  const navMenu = document.getElementById("navMenu");
+  navMenu.style.display === "flex" ? "none" : "flex";
+}
+
+// Adicione um listener para fechar o menu ao clicar fora dele
+document.addEventListener("click", function (event) {
+  const navMenu = document.getElementById("navMenu");
+  const menuToggle = document.querySelector(".menu-toggle");
+
+  if (!navMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+    navMenu.style.display = "none";
+  }
+});
